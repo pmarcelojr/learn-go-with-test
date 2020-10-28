87bc2df4de0823ab141e26f75fafce525450c320
pmarcelojr <willian.fcamara@fornecedores.klabin.com.br>
2020-10-28 18:33:45 -0300

    primeiros-passos/ola-mundo/v4

---
+++ primeiros-passos/ola-mundo/v4/ola.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+const prefixoOlaPortugues = "Olá, "
+
+func ola(nome string) string {
+	return prefixoOlaPortugues + nome
+}
+
+func main() {
+	fmt.Println(ola("Marcelo"))
+}
